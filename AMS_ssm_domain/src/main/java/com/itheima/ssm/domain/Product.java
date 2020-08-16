d82da33fee92f504a594c743d77df5c21895be9d
Meng Joe <MengJoe@users.noreply.github.com>
2020-08-16 10:47:49 +0800

    在AMS_ssm_domain中创建Product类

---
+++ AMS_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -0,0 +1,16 @@
+package com.itheima.ssm.domain;
+
+import java.util.Date;
+
+public class Product {
+    private String id; // 主键
+    private String productNum; // 编号 唯一
+    private String productName; // 名称
+    private String cityName; // 出发城市
+    private Date departureTime; // 出发时间
+    private String departureTimeStr;
+    private double productPrice; // 产品价格
+    private String productDesc; // 产品描述
+    private Integer productStatus; // 状态 0 关闭 1 开启
+    private String productStatusStr;
+}
