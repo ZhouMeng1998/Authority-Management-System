1a15cf60198bcc669029e669497136f868df32f7
Meng Joe <MengJoe@users.noreply.github.com>
2020-08-18 23:07:53 +0800

    初识spring-security配置

---
+++ AMS_ssm_service/src/main/java/com/itheima/ssm/service/IUserService.java
@@ -0,0 +1,8 @@
+package com.itheima.ssm.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface IUserService extends UserDetailsService {
+}
